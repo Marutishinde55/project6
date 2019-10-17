10e5b82c8cc6ec1c2aed0e6c3fc1bb05fad8e706
HP <HP@DESKTOP-M51MOEK>
2019-10-17 16:44:31 +0530

    Structure of Project

--- src/main/java/com/jbk/controller/RegisterController.java
+++ src/main/java/com/jbk/controller/RegisterController.java
@@ -1,6 +1,12 @@
 package com.jbk.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class RegisterController 
 {
- 
+    public String Register() 
+    {
+    	return "Welcome";
+    }
 }
